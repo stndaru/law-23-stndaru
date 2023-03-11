b20c60ee415f19609aa49862c26652598ad9eb20
stndaru <stefanus.ndaru@ui.ac.id>
2023-03-12 02:07:40 +0700

    Added random song implementation and partially voice recognition

--- audcentral/urls.py
+++ audcentral/urls.py
@@ -7,7 +7,9 @@ app_name = 'audcentral'
 urlpatterns = [
     path('', views.main, name='base'),
     path('top-tracks-region', views.getTopTrackRegion, name='base'),
-    path('top-tracks-region/<req>', views.getTopTrackRegionDataFormat, name='base'),
+    path('top-tracks-region/json', views.getTopTrackRegionJSON, name='base'),
     path('identify-track', views.identifyTrackUpload, name='base'),
     path('identify-track/json', views.identifyTrackUploadJSON, name='base'),
+    path('search-name/json/<name>', views.getTrackDetailsJSON, name='base'),
+    path('random/json', views.randomSongJSON)
 ]
--- audcentral/views.py
+++ audcentral/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from .models import Document
-import json, requests, mutagen
+import json, requests, mutagen, random
 from django.views.decorators.csrf import csrf_exempt
 
 LASTFM_TOKEN = "14e1db88026c7814d4ed01afac4f1e6b"
 AUD_TOKEN = "52d936452cec3fa3b2127034a38febc6" # Expire 24 March
+GENIUS_TOKEN = "8DnZUqI2sO53dmGaKQ-QdB7efIsIG10xKZGCOEqbDuuveLyRgYayIzblR5aUlQL0"
 
 # Create your views here.
 def main(request):
@@ -24,6 +25,19 @@ def identifyTrackUpload(request):
             audio_info = mutagen.File(audio).info
 
             if audio_info.length < 20 and audio.size < 19971520:
+
+                data = {
+                    'api_token': AUD_TOKEN,
+                    'return': 'apple_music,spotify',
+                }
+
+                files = {
+                    'file': audio,
+                }
+
+                # result = requests.post('https://api.audd.io/', data=data, files=files)
+                # print(result.text)
+
                 status = "Success"
 
         return render(request, "identifyTrack.html", {"audio_info":audio_info, "status": status})
@@ -34,29 +48,14 @@ def identifyTrackUpload(request):
 def identifyTrackUploadJSON(request):
 
     status = "Error"
-    audio_info = "None"
+    audio_info = "None"    
 
-    if request.method=="POST":
+    context = {
+        audio_info : audio_info,
+        status: status,
+    }
 
-        validity = request.POST.get('audio', False)
-        
-        if validity:
-            audio = request.FILES["audio"]
-
-            audio_info = mutagen.File(audio).info
-
-            if audio_info.length < 20 and audio.size < 19971520:
-                status = "Success"
-
-        context = {
-            audio_info : audio_info,
-            status: status,
-        }
-
-        return HttpResponse(json.dumps(context), content_type="application/json")
-    
-    return HttpResponseBadRequest
-
+    return HttpResponse(json.dumps(context), content_type="application/json")
 
 
 def getTopTrackRegion(request):
@@ -67,20 +66,54 @@ def getTopTrackRegion(request):
 
     return render(request, 'topTracks.html', {'result': json_data})
 
-
-def getTopTrackRegionDataFormat(request, req):
+def getTopTrackRegionJSON(request):
     country_id = "spain"
     response = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country={country_id}&api_key={LASTFM_TOKEN}&format=json")
     data = json.loads(response.text)
 
-    print(data)
+    return HttpResponse(json.dumps(data), content_type="application/json")
     
-    if req == "JSON":
-        # TODO
-        return HttpResponse(json.dumps(data), content_type="application/json")
     
-    elif req == "" or None:
-        return HttpResponseRedirect("")
+def getTrackDetailsJSON(request, name):
+    genius_request = "https://api.genius.com/search?q=" + name
+    genius = requests.get(genius_request, headers={'Authorization': 'Bearer ' + GENIUS_TOKEN})
+    data = json.loads(genius.text)
+
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+def randomSongJSON(request):
+    song_id = random.randint(1,10000000)
+    genius_data = "None"
+    chosen_song_lastFM = "None"
+    lastFM_data_detailed = "None"
+
+
+    genius_request = "https://api.genius.com/songs/" + str(song_id)
+    genius = requests.get(genius_request, headers={'Authorization': 'Bearer ' + GENIUS_TOKEN})
+    genius_data = json.loads(genius.text)
+
+    if genius_data['meta']['status'] == 404:
+        error = {"Server Error" : "Unfortunately you weren't lucky with the roll. Please try again."}
+        return HttpResponse(json.dumps(error), content_type="application/json")
     
-    else:
-        return HttpResponseBadRequest
+    song_title = genius_data["response"]["song"]["title"]
+
+    lastFM = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=track.search&track={song_title}&api_key={LASTFM_TOKEN}&format=json")
+    lastFM_data = json.loads(lastFM.text)
+
+    if len(lastFM_data['results']['trackmatches']['track']) > 0:
+        chosen_song_lastFM = lastFM_data['results']['trackmatches']['track'][0]
+
+        LFM_song_title = chosen_song_lastFM['name']
+        LFM_song_artist = chosen_song_lastFM['artist']
+
+        lastFM_detailed_song = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={LASTFM_TOKEN}&artist={LFM_song_artist}&track={LFM_song_title}&format=json")
+        lastFM_data_detailed = json.loads(lastFM_detailed_song.text)
+
+    data = {
+        "genius_data" : genius_data,
+        "lastFM_data": chosen_song_lastFM,
+        "lastFM_data_detailed": lastFM_data_detailed,
+    }
+
+    return HttpResponse(json.dumps(data), content_type="application/json")
