4c50678fa74c41e1f106803fdbe309856322737d
stndaru <stefanus.ndaru@ui.ac.id>
2023-03-31 07:36:29 +0000

    Add database model for transcription

--- audcentral/models.py
+++ audcentral/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Document(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     file = models.FileField()
+
+class TranscriptResult(models.Model):
+    transcribe_result = models.TextField()
+    translate_result = models.TextField()
+    sentiment_result = models.CharField(max_length=500)
